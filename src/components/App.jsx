c685fe8f4165e57185e7054d895327ef0f3c42bd
Yaroslav Grabovych <ygrabovych@gmail.com>
2023-08-07 23:15:41 +0300

    adding input handle

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,13 +4,26 @@ export class App extends React.Component {
   state = {
     contacts: [],
     name: '',
+    number: '',
   };
 
   handleNameInput = e => {
     const { name, value } = e.currentTarget;
 
-    this.setState({ [name]: value });
-    console.log(this.state.name)
+    this.setState({ [name]: value }, () => {
+      console.log(this.state);
+    });
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    const { name, number } = this.state;
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, { name, number }],
+      name: '',
+      number: '',
+    }), () => {
+      console.log(this.state.contacts);
+    });
   };
 
   render() {
@@ -36,8 +49,21 @@ export class App extends React.Component {
               required
               onChange={this.handleNameInput}
             />
+            <input
+              type="tel"
+              name="number"
+              value={this.state.number}
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+              onChange={this.handleNameInput}
+            />
           </label>
+          <button type="submit" onClick={this.handleSubmit}>Додати</button>
         </form>
+        <h2>Contacts</h2>
+        <p>Name <span>{this.state.contacts.name}</span></p>
+        <p>Number <span>{this.state.contacts.number}</span></p>
       </div>
     );
   }
